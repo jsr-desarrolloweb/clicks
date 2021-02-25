b042cf48993237583b2291e14ea445354be48b4f
jsr <jsr.desarrolloweb@gmail.com>
2021-02-25 19:24:34 +0100

    Configuracion iniciald el proyecto

---
+++ src/main/java/api/clicks/ClicksApplication.java
@@ -0,0 +1,13 @@
+package api.clicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClicksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/clicks/ClicksApplicationTests.java
@@ -0,0 +1,13 @@
+package api.clicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
