ae4468f2af3150c5d6df8a88254e83c348498357
jsr <jsr.desarrolloweb@gmail.com>
2021-03-02 22:43:42 +0100

    ImageController/ImageService

--- src/main/java/api/clicks/bootstrap/Seeder.java
+++ src/main/java/api/clicks/bootstrap/Seeder.java
@@ -25,37 +25,6 @@ public class Seeder implements CommandLineRunner {
 
     @Override
     public void run(String[] args) {
-/*        //TEAMS
-        Team t1 = new Team("Los locos", 200);
-        Team t2 = new Team("Las marihuanas", 234);
-        teamRepository.save(t1);
-        teamRepository.save(t2);
-
-        //Set de equipos para los jugadores
-        Set<Team> arrayTeams = new HashSet<>();
-        arrayTeams.add(t1);
-        arrayTeams.add(t2);
-
-        //PROVINCES
-        Province prov1 = new Province("Cadiz", 0);
-        Province prov2 = new Province("Sevilla", 0);
-        provinceRepository.save(prov1);
-        provinceRepository.save(prov2);
-
-
-        //LOCALITIES
-        Locality l1 = new Locality("Jerez", 0);
-        Locality l2 = new Locality("Medina", 2);
-        localityRepository.save(l1);
-        localityRepository.save(l2);
-
-
-        //PLAYERS
-        Player p1 = new Player("jaime", "pestillo", null, 10, arrayTeams, l1);
-        Player p2 = new Player("cristina", "pestillo", null, 8, null, l2);
-        playerRepository.save(p1);
-        playerRepository.save(p2);*/
-
 
         Country espania = new Country("España", 0);
         countryRepository.save(espania);
---
+++ src/main/java/api/clicks/services/ImageService.java
@@ -0,0 +1,37 @@
+package api.clicks.services;
+
+import api.clicks.models.Player;
+import api.clicks.repositories.PlayerRepository;
+import api.clicks.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class ImageService {
+    @Autowired
+    private PlayerRepository playerRepository;
+    @Autowired
+    private TeamRepository teamRepository;
+
+
+    public boolean imageStore(MultipartFile file, Long id) throws IOException {
+        String myFileName = id.toString()+" - "+file.getOriginalFilename();
+        Path targetPath = Paths.get("./images/"
+                +myFileName)
+                .normalize();
+        Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+        Player player = playerRepository.findById(id)
+                                        .orElseThrow(()-> new EntityNotFoundException(id.toString()));
+        player.setAvatar("/download/"+myFileName);
+        playerRepository.save(player);
+        return true;
+    }
+}
